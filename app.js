95dabab706a4f672c63f18716b58ff4b31f263e8
ljbassa <ljbassa@kaist.ac.kr>
2025-01-04 09:47:16 +0000

    home

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const { kakaoAuthRoutes } = require('./src/kakaoAuth');
+const homeRoutes = require('./src/home/homeRoutes');
 require('dotenv').config();
 
 const app = express();
+
+// Body-parser 설정
+app.use(express.json());
 
 // 카카오 로그인 라우터 등록
 app.use('/auth/kakao', kakaoAuthRoutes);
 
 //사용자 관리
-//app.use('/users', userRoutes);
+app.use('/users', userRoutes);
+
+//홈 탭 ???
+app.use('/home', homeRoutes);
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
